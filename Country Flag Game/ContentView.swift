4cf5e20b54866958ae9282a5e39bde709548f0ff
Jack <Jwenschhof@bsd220.org>
2023-01-20 10:48:48 -0600

    Set up content view

--- Country Flag Game/ContentView.swift
+++ Country Flag Game/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+ //   @StateObject var quizManager = QuizManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("Guess That Country!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                NavigationLink {
+             //       QuizView()
+             //           .environmentObject(quizManager)
+                } label: {
+              //      PrimaryButton(text: "Start")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+            .background(Color(hue: 0.599, saturation: 0.433, brightness: 0.971))
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
